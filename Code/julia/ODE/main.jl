d8bbb29942151c287a7e2b50bda277fbd528603b
Ali R. Vahdati <arv.kavir@gmail.com>
2022-08-12 18:48:34 +0200

    Implement time-varying exploitation

--- Code/julia/ODE/main.jl
+++ Code/julia/ODE/main.jl
@@ -10,7 +10,7 @@ using CSV
 using DiffEqParamEstim
 using Optim
 
-## TODO: exploitation stops during certain months of the year. Implement time varying E.
+## [x]: exploitation stops during certain months of the year. Implement time varying E.
 ## TODO: From topography, set distance between two sites to infinite, if there is land/another site between them
 ## TODO: Empirically estimate the rate at which individuals from one stage turn into another one
 ## TODO: Create another version of model where there are five life stages instead of two. In this system, the probability to migrate decreases exponentially. $1/d \times e^{-\gamma}$.
@@ -31,18 +31,19 @@ function single_site!(du, u, p, t)
   Nⱼ, Nₐ, Sₐ = u
   r, g, dⱼ, dₐ, E, K, size_growth_rate, sizeₘₐₓ = p
   du[1] = dNⱼ = (r * Nₐ * ((K - Nₐ) / K)) - (dⱼ * Nⱼ) - (g * Nⱼ)
-  du[2] = dNₐ = (g * Nⱼ) - (dₐ * Nₐ) - (E * Nₐ)
-  du[3] = dSₐ = size_growth_rate * Sₐ * (1 - Sₐ / (sizeₘₐₓ - (sizeₘₐₓ * E)))
+  du[2] = dNₐ = (g * Nⱼ) - (dₐ * Nₐ) - (E(t) * Nₐ)
+  du[3] = dSₐ = size_growth_rate * Sₐ * (1 - Sₐ / (sizeₘₐₓ - (sizeₘₐₓ * E(t))))
 end
 
-p_1 = [0.6, 0.6, 0.05, 0.08, 0.2, 1e4, 0.2, 40.0]
+Et(t) = (sin(t)^2)/2  # time varying exploitation
+p_1 = [0.6, 0.06, 0.05, 0.08, Et, 1e4, 0.2, 40.0]
 u0_1 = [1e3, 1e3, 40.0]
 tspan_1 = (0.0, 50.0)
 prob_1 = ODEProblem(single_site!, u0_1, tspan_1, p_1)
 sol_1 = solve(prob_1, Tsit5())
 
-plot(sol_1, vars = (0, 1), label = "Nⱼ")
-plot!(sol_1, vars = (0, 2), label = "Nₐ")
+plot(sol_1, vars=(0, 1), label="Nⱼ")
+plot!(sol_1, vars=(0, 2), label="Nₐ")
 
 ### ----------------------------------------------------------------
 ### 2. Two sites
@@ -86,13 +87,13 @@ tspan_2 = (0.0, 100.0)
 prob_2 = ODEProblem(two_sites!, u0_2, tspan_2, p_2)
 sol_2 = solve(prob_2, Tsit5())
 
-plot(sol_2, vars = (0, 1), label = "Nⱼ₁")
-plot!(sol_2, vars = (0, 2), label = "Nₐ₁")
-plot!(sol_2, vars = (0, 4), label = "Nⱼ₂")
-plot!(sol_2, vars = (0, 5), label = "Nₐ₂")
+plot(sol_2, vars=(0, 1), label="Nⱼ₁")
+plot!(sol_2, vars=(0, 2), label="Nₐ₁")
+plot!(sol_2, vars=(0, 4), label="Nⱼ₂")
+plot!(sol_2, vars=(0, 5), label="Nₐ₂")
 
-plot(sol_2, vars = (0, 3), label = "S₁")
-plot!(sol_2, vars = (0, 6), label = "S₂")
+plot(sol_2, vars=(0, 3), label="S₁")
+plot!(sol_2, vars=(0, 6), label="S₂")
 
 ### --------------------------------------------------
 ### 3. Sensitivity analysis
@@ -101,49 +102,49 @@ plot!(sol_2, vars = (0, 6), label = "S₂")
 ### 3.1 Single site
 
 f1 = function (p)
-  prob1 = remake(prob_1; p = p)
-  sol = solve(prob1, Tsit5(); saveat = tspan_1)
+  prob1 = remake(prob_1; p=p)
+  sol = solve(prob1, Tsit5(); saveat=tspan_1)
   [sol[1, end], sol[2, end], sol[3, end]]
 end
 
 bounds_1 = [[0.0, 1.0], [0.0, 1.0], [0.0, 1.0], [0.0, 1.0], [0.0, 1.0], [1e4, 1e5], [0.0, 1.0], [55.0, 55.0]]
 
-m = gsa(f1, Sobol(), bounds_1, N = 10000)
+m = gsa(f1, Sobol(), bounds_1, N=10000)
 
 param_names = ["r", "g", "dⱼ", "dₐ", "E", "K", "size_growth_rate", "sizeₘₐₓ"]
 
-p1 = bar(param_names, m.S1[1, :], title = "First order Nj", legend = false, xrotation = 60)
-p2 = bar(param_names, m.ST[1, :], title = "Total order Nj", legend = false, xrotation = 60)
-p3 = bar(param_names, m.S1[2, :], title = "First order Na", legend = false, xrotation = 60)
-p4 = bar(param_names, m.ST[2, :], title = "Total order Na", legend = false, xrotation = 60)
-p5 = bar(param_names, m.S1[3, :], title = "First order S", legend = false, xrotation = 60)
-p6 = bar(param_names, m.ST[3, :], title = "Total order S", legend = false, xrotation = 60)
+p1 = bar(param_names, m.S1[1, :], title="First order Nj", legend=false, xrotation=60)
+p2 = bar(param_names, m.ST[1, :], title="Total order Nj", legend=false, xrotation=60)
+p3 = bar(param_names, m.S1[2, :], title="First order Na", legend=false, xrotation=60)
+p4 = bar(param_names, m.ST[2, :], title="Total order Na", legend=false, xrotation=60)
+p5 = bar(param_names, m.S1[3, :], title="First order S", legend=false, xrotation=60)
+p6 = bar(param_names, m.ST[3, :], title="Total order S", legend=false, xrotation=60)
 
 plot(p1, p3, p2, p4) # juvenile and adult
-plot(p5,p6) # body size
+plot(p5, p6) # body size
 
 ### 3.2 Two sites 
 
 f2 = function (p)
-  prob1 = remake(prob_2; p = p)
-  sol = solve(prob1, Tsit5(); saveat = tspan_2)
+  prob1 = remake(prob_2; p=p)
+  sol = solve(prob1, Tsit5(); saveat=tspan_2)
   [sol[1, end], sol[2, end], sol[4, end], sol[5, end]]
 end
 
 bounds_2 = [[0.0, 1.0], [0.0, 1.0], [0.0, 1.0], [0.0, 1.0], [0.0, 1.0], [0.0, 1.0], [0.0, 1.0], [0.0, 1.0], [0.0, 1.0], [1e4, 1e5], [1e4, 1e5], [55.0, 55.0], [55.0, 55.0]]
 
-m = gsa(f2, Sobol(), bounds_2, N = 10000)
+m = gsa(f2, Sobol(), bounds_2, N=10000)
 
 param_names = ["r", "g", "dⱼ", "dₐ", "size_growth_rate", "mₒᵤₜ₁", "mₒᵤₜ₂", "E₁", "E₂", "K₁", "K₂", "sizeₘₐₓ₁", "sizeₘₐₓ₂"]
 
-p1 = bar(param_names, m.S1[1, :], title = "First order Nj1", legend = false, xrotation = 60)
-p2 = bar(param_names, m.ST[1, :], title = "Total order Nj1", legend = false, xrotation = 60)
-p3 = bar(param_names, m.S1[2, :], title = "First order Na1", legend = false, xrotation = 60)
-p4 = bar(param_names, m.ST[2, :], title = "Total order Na1", legend = false, xrotation = 60)
-p5 = bar(param_names, m.S1[3, :], title = "First order Nj2", legend = false, xrotation = 60)
-p6 = bar(param_names, m.ST[3, :], title = "Total order Nj2", legend = false, xrotation = 60)
-p7 = bar(param_names, m.S1[4, :], title = "First order Na2", legend = false, xrotation = 60)
-p8 = bar(param_names, m.ST[4, :], title = "Total order Na2", legend = false, xrotation = 60)
+p1 = bar(param_names, m.S1[1, :], title="First order Nj1", legend=false, xrotation=60)
+p2 = bar(param_names, m.ST[1, :], title="Total order Nj1", legend=false, xrotation=60)
+p3 = bar(param_names, m.S1[2, :], title="First order Na1", legend=false, xrotation=60)
+p4 = bar(param_names, m.ST[2, :], title="Total order Na1", legend=false, xrotation=60)
+p5 = bar(param_names, m.S1[3, :], title="First order Nj2", legend=false, xrotation=60)
+p6 = bar(param_names, m.ST[3, :], title="Total order Nj2", legend=false, xrotation=60)
+p7 = bar(param_names, m.S1[4, :], title="First order Na2", legend=false, xrotation=60)
+p8 = bar(param_names, m.ST[4, :], title="Total order Na2", legend=false, xrotation=60)
 
 plot(p1, p3, p2, p4) # Pop 1, juvenile and adult
 plot(p5, p7, p6, p8) # Pop 2, juvenile and adult
@@ -161,9 +162,9 @@ sitedfs = groupby(df, "sampling_site")
 
 size_by_year_all_sites = DataFrame[]
 for site in 1:length(sitedfs)
-  site1year = groupby(sitedfs[site], [:year, :species]);
+  site1year = groupby(sitedfs[site], [:year, :species])
   # select(site1year, :total_length_mm)
-  cc = combine(site1year, :total_length_mm .=> [mean,std, median])
+  cc = combine(site1year, :total_length_mm .=> [mean, std, median])
   push!(size_by_year_all_sites, cc)
 end
 
@@ -214,6 +215,6 @@ x = 0:0.005:1
 y1(x) = x * (1 - x)
 y2(x) = x^(2 / 3) - x
 y3(x) = -x * log(x)
-plot(x, y1.(x), label = "Logistic", xlabel = "Size", ylabel = "Growth rate")
-plot!(x, y2.(x), label = "von Bertalanffy")
-plot!(x, y3.(x), label = "Gompertz")
+plot(x, y1.(x), label="Logistic", xlabel="Size", ylabel="Growth rate")
+plot!(x, y2.(x), label="von Bertalanffy")
+plot!(x, y3.(x), label="Gompertz")
