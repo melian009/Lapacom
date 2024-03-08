eafdb5e3cfd46940f5d07f1dd6a2513273e80284
Eros Fernando Geppi <eros.f.geppi@gmail.com>
2024-03-08 09:42:46 +0000

    Update X_scenarios.jl

--- Code/julia/ODE/Numerical_approximation/X_scenarios.jl
+++ Code/julia/ODE/Numerical_approximation/X_scenarios.jl
@@ -1,5 +1,6 @@
 #Packages
 
+
 using Pkg
 Pkg.activate(".")
 using LinearAlgebra
@@ -104,16 +105,44 @@ end
 Hs=length(zeros(Float64,size(1:length(H_span))))
 cijs = length(zeros(Float64,size(1:length(cij_span))))
 
-Na1c0 = zeros(Hs,t_l)
+
+H1_ = H_span[1]
+cij_= cij_span[1]
+
+p_span = [t0_, k_, r_, K_, H1_, d, Smax_, size_growth_rate,cij_,Naj_]  
+n=10  #Number of years in the simulation
+tspan = (0,365.14*n)
+U0_ = [10^4, 49.25]
+prob_ = ODEProblem(SLC!, U0_, tspan, p_span)
+solve_= solve(prob_, Tsit5())
+
+t_l = length(zeros(Float64,size(1:length(solve_.u))))
+n_v = length(zeros(Float64,size(1:length(solve_.u[1]))))
+vars = zeros(t_l,n_v)
+time2 = zeros(t_l,1)
+
+  for j in 1:length(solve_.u)
+    for i in 1:2
+      vars[j,i] = solve_.u[j][i]
+      time2[j] = solve_.t[j]
+    end 
+  end
+    resultados_simulaciones = zeros(length(time2), length(H_span), length(cij_span)) 
+    longitud_simulacion = length(time2)
+
+
+
+
+
+
 
 
 
 
 for i in 1:length(H_span)
-#i=2
   H1_ = H_span[i]
   for j in 1:length(cij_span)
-#j=1
+
    cij_= cij_span[j]
 
     p_span = [t0_, k_, r_, K_, H1_, d, Smax_, size_growth_rate,cij_,Naj_]  
@@ -128,32 +157,32 @@ for i in 1:length(H_span)
     vars = zeros(t_l,n_v)
     time2 = zeros(t_l,1)
  
-    for j in 1:length(solve_.u)
-      for i in 1:2
-        vars[j,i] = solve_.u[j][i]
-        time2[j] = solve_.t[j]
-      end 
-    end
+      for j in 1:length(solve_.u)
+        for i in 1:2
+          vars[j,i] = solve_.u[j][i]
+          time2[j] = solve_.t[j]
+        end 
+      end
 
-  if i == 1 && j == 1
-    resultados_simulaciones = zeros(length(time2), length(cij_span), length(H_span)) 
-    longitud_simulacion = length(time2)
+    if i == 1 && j == 1
+      longitud_simulacion = length(time2)
     # Guardar los resultados de la simulación en la matriz
-    resultados_simulaciones[:,i,1] = vars[:,1]
+      resultados_simulaciones[:,i,j] = vars[:,1]
     
-  else i!=1
-    lon_lar = length(resultados_simulaciones[:,1,1])
-    lon_cor = length(vars[:,1])
-    elementos_faltantes = lon_lar - lon_cor
-    if elementos_faltantes > 0
-      vector_corto = vcat(vars[:,1],zeros(elementos_faltantes))
-    else
-      vector_corto = vars[:,1][1:lon_lar] # Trunca el vector corto si es más largo que el vector largo
+    else 
+      lon_lar = length(resultados_simulaciones[:,1,1])
+      lon_cor = length(vars[:,1])
+      elementos_faltantes = lon_lar - lon_cor
+      if elementos_faltantes > 0
+        vector_corto = vcat(vars[:,1],zeros(elementos_faltantes))
+      else
+        vector_corto = vars[:,1][1:lon_lar] # Trunca el vector corto si es más largo que el vector largo
+      end
+    resultados_simulaciones[:,i,j] = vector_corto
     end
-    resultados_simulaciones[:,j,i] = vector_corto
   end
 end
-end
+
 
 Na1c0=[resultados_simulaciones[:,1,1]  resultados_simulaciones[:,5,1]  resultados_simulaciones[:,11,1]]
 Na5c0=[resultados_simulaciones[:,1,5],resultados_simulaciones[:,5,5], resultados_simulaciones[:,11,5]]
@@ -164,16 +193,16 @@ resultados_simulaciones[:,1,1]
 length(resultados_simulaciones[:,1,5])
 
 
-#X=0 en el dia 625
-X0 = resultados_simulaciones[625,1,:]
-X1 = resultados_simulaciones[625,6,:]
-X2 = resultados_simulaciones[625,10,:]
-X3 = resultados_simulaciones[625,11,:]
-#X=0 en el dia 900
-X4 = resultados_simulaciones[900,1,:]
-X5 = resultados_simulaciones[900,6,:]
-X6 = resultados_simulaciones[900,10,:]
-X7 = resultados_simulaciones[900,11,:]
+#X=0 en el dia 625 (day, H, cij)
+X0 = resultados_simulaciones[625,:,1]
+X1 = resultados_simulaciones[625,:,6]
+X2 = resultados_simulaciones[625,:,10]
+X3 = resultados_simulaciones[625,:,11]
+#X=1 en el dia 900
+X4 = resultados_simulaciones[900,:,1]
+X5 = resultados_simulaciones[900,:,6]
+X6 = resultados_simulaciones[900,:,10]
+X7 = resultados_simulaciones[900,:,11]
 
 
 plot(H_span,X0,label="cij=0.0", color=:blue)
